feb48b2959f38957ad71cf76e822474c55f31892
christianfirecup <chris@10.249.2.94>
2021-12-02 10:25:27 -0500

    ADDED RESTVALUE COMMAD FOR ALL MADE DISC TO THIS POINT

--- src/main/java/net/mcreator/perplayeritem/procedures/ResetvaluesCommandExecutedProcedure.java
+++ src/main/java/net/mcreator/perplayeritem/procedures/ResetvaluesCommandExecutedProcedure.java
@@ -41,5 +41,160 @@ public class ResetvaluesCommandExecutedProcedure {
 				capability.syncPlayerVariables(entity);
 			});
 		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d3 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d3p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d4 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d4p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d5 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d5p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d6 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d6p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d7 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d7p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d8 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d8p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d9 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d9p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d10 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d10p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d11 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d11p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d12 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d12p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d13p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d13 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		Perplayeritem1ModVariables.MapVariables.get(world).d14 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d14p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d15 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d15p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d16 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d16p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d17 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d17p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d18 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d18p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d19 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
+		{
+			double _setval = (double) 0;
+			entity.getCapability(Perplayeritem1ModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+				capability.d19p = _setval;
+				capability.syncPlayerVariables(entity);
+			});
+		}
+		Perplayeritem1ModVariables.MapVariables.get(world).d20 = (double) 0;
+		Perplayeritem1ModVariables.MapVariables.get(world).syncData(world);
 	}
 }
